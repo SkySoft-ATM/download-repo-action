4636ebeaf9daa4cf265a4d9c49fd1be48d13d7c6
Damien C <damien.chambon@skysoft-atm.com>
2019-12-18 11:22:14 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # download-repo-action
+
+[![Actions Status](https://github.com/SkySoft-ATM/download-repo-action/workflows/snapshot/badge.svg)](https://github.com/SkySoft-ATM/download-repo-action/actions)
+
+[![Actions Status](https://github.com/SkySoft-ATM/download-repo-action/workflows/release/badge.svg)](https://github.com/SkySoft-ATM/download-repo-action/actions)
+
+This action makes it easy to quickly download a github repository in your workflow 
+using the GitHub API.
+
+In order to use this action, a set of elements need to be provided. 
+
+**Note** This action is still "Release Candidate" and the API may change in
+future versions. 🙂
+
+## Examples
+
+### Download a repository as (zip) archive file
+
+```yaml
+on: [push]
+
+jobs:
+  retrieve:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: SkySoft-ATM/download-repo-action@master
+        with:
+          github-token: ${{secrets.GITHUB_TOKEN}}
+          owner: "SkySoft-ATM"
+          repo: "my-repo"
+          branch: "master"
+          dir: "./"          
+```
---
+++ index.js
@@ -0,0 +1,58 @@
+const core = require('@actions/core');
+const { GitHub, context } = require('@actions/github');
+const path = require('path');
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+async function run() {
+  try {
+    const token = core.getInput('github-token', { required: true });
+    const debug = core.getInput('debug');
+    const owner = core.getInput('owner', { required: true });
+    const repo = core.getInput('repo', { required: true });
+    const branch = core.getInput('branch');
+    const directory = core.getInput('dir');
+    const opts = {}
+    if (debug === 'true') opts.log = console;
+
+    core.info(`Owner: ${owner}`);
+    core.info(`Repo: ${repo}`);
+    core.info(`Directory: ${directory}`);
+
+    const zipPath = path.resolve(directory, `${branch}.zip`);
+
+    await download(token, owner, repo, branch, zipPath);
+    console.log(`::set-output name=file::${zipPath}`);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+async function download(token, owner, repo, branch, zipPath) {
+
+  const options = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/octet-stream'
+    }
+  };
+
+  const url = `https://github.com/${owner}/${repo}/archive/${branch}.zip?access_token=${token}`;
+  fetch(url, options)
+    .then(checkStatus)
+    .then(res => {
+      const dest = fs.createWriteStream(zipPath);
+      res.body.pipe(dest);
+    });
+}
+
+function checkStatus(res) {
+  if (res.ok) {
+    return res;
+  } else {
+    throw new Error(res.statusText);
+  }
+}
+
+run();
