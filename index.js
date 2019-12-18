46786941975905e547f9e6f0bf3e53dacfe2ea44
Damien C <damien.chambon@skysoft-atm.com>
2019-12-18 11:27:32 +0100

    handler error in async func

--- index.js
+++ index.js
@@ -5,54 +5,58 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 
 async function run() {
-  try {
-    const token = core.getInput('github-token', { required: true });
-    const debug = core.getInput('debug');
-    const owner = core.getInput('owner', { required: true });
-    const repo = core.getInput('repo', { required: true });
-    const branch = core.getInput('branch');
-    const directory = core.getInput('dir');
-    const opts = {}
-    if (debug === 'true') opts.log = console;
+    try {
+        const token = core.getInput('github-token', { required: true });
+        const debug = core.getInput('debug');
+        const owner = core.getInput('owner', { required: true });
+        const repo = core.getInput('repo', { required: true });
+        const branch = core.getInput('branch');
+        const directory = core.getInput('dir');
+        const opts = {}
+        if (debug === 'true') opts.log = console;
 
-    core.info(`Owner: ${owner}`);
-    core.info(`Repo: ${repo}`);
-    core.info(`Directory: ${directory}`);
+        core.info(`Owner: ${owner}`);
+        core.info(`Repo: ${repo}`);
+        core.info(`Directory: ${directory}`);
 
-    const zipPath = path.resolve(directory, `${branch}.zip`);
+        const zipPath = path.resolve(directory, `${branch}.zip`);
 
-    await download(token, owner, repo, branch, zipPath);
-    console.log(`::set-output name=file::${zipPath}`);
+        await download(token, owner, repo, branch, zipPath);
+        console.log(`::set-output name=file::${zipPath}`);
 
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 async function download(token, owner, repo, branch, zipPath) {
 
-  const options = {
-    method: 'GET',
-    headers: {
-      Accept: 'application/octet-stream'
-    }
-  };
+    const options = {
+        method: 'GET',
+        headers: {
+            Accept: 'application/octet-stream'
+        }
+    };
 
-  const url = `https://github.com/${owner}/${repo}/archive/${branch}.zip?access_token=${token}`;
-  fetch(url, options)
-    .then(checkStatus)
-    .then(res => {
-      const dest = fs.createWriteStream(zipPath);
-      res.body.pipe(dest);
-    });
+    const url = `https://github.com/${owner}/${repo}/archive/${branch}.zip?access_token=${token}`;
+    fetch(url, options)
+        .then(checkStatus)
+        .then(res => {
+            const dest = fs.createWriteStream(zipPath);
+            res.body.pipe(dest);
+        });
 }
 
 function checkStatus(res) {
-  if (res.ok) {
-    return res;
-  } else {
-    throw new Error(res.statusText);
-  }
+    if (res.ok) {
+        return res;
+    } else {
+        throw new Error(res.statusText);
+    }
+}
+function handleError(err) {
+    console.error(err)
+    core.setFailed(err.message)
 }
 
-run();
+run().catch(handleError);
